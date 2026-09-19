3e04ef168cb37ad5509cc2538d696e80b6b4f170
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2026-09-19 16:34:26 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,83 @@
 import logo from "../assets/images/logo.png";
+import {
+  Home,
+  Info,
+  Briefcase,
+  Building2,
+  MapPin,
+  Phone,
+  Mail,
+  CopyrightIcon,
+  TextAlignCenter,
+} from "lucide-react";
 
 function Footer() {
   return (
     <footer className="footer">
-
+      {/* Logo */}
       <div>
         <img src={logo} alt="Cosmic Infra" className="footer-logo" />
       </div>
 
+      {/* Quick Links */}
       <div>
         <h3>Quick Links</h3>
-        <p>Home</p>
-        <p>About</p>
-        <p>Services</p>
-        <p>Properties</p>
+
+        <p>
+          <Home size={16} />
+          <span>Home</span>
+        </p>
+
+        <p>
+          <Info size={16} />
+          <span>About</span>
+        </p>
+
+        <p>
+          <Briefcase size={16} />
+          <span>Services</span>
+        </p>
+
+        <p>
+          <Building2 size={16} />
+          <span>Properties</span>
+        </p>
       </div>
 
+      {/* Contact Us */}
       <div>
         <h3>Contact Us</h3>
-        <p>Dubai, UAE</p>
-        <p>+971 123456789</p>
-        <p>info@cosmicinfra.com</p>
+
+        <p>
+          <MapPin size={16} />
+          <span>
+            Empire Heights, Tower A, 7th Floor, Unit 01, Business Bay, Dubai,
+            UAE
+          </span>
+        </p>
+
+        <p>
+          <Phone size={16} />
+          <span>+971123456789</span>
+        </p>
+
+        <p>
+          <Mail size={16} />
+          <span>info@cosmicinfra.com</span>
+        </p>
       </div>
 
+      {/* Newsletter */}
       <div>
         <h3>Newsletter</h3>
         <input placeholder="Your email address" />
         <button>Sign up</button>
       </div>
+
+      <p className="copyright">
+        <CopyrightIcon />
+        <span>2026 All Rights Reserved</span>
+      </p>
 
     </footer>
   );
